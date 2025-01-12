bfd86ecdf46bc83ed2fe319e0bc1e4212d2a0613
一人一口粥 <1552951165@qq.com>
2025-01-12 22:26:37 +0800

    230.二叉搜索树中第 K 小的元素

--- src/LeetCode/二叉树/t230.java
+++ src/LeetCode/二叉树/t230.java
@@ -2,6 +2,8 @@ package LeetCode.二叉树;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author ZhouYihe 1552951165@qq.com
@@ -20,7 +22,55 @@ public class t230 {
      * 输入：root = [5,3,6,2,4,null,null,1], k = 3
      * 输出：3
      */
+    // 方法二：记录子树的结点数
     public int kthSmallest(TreeNode root, int k) {
+        MyBst bst = new MyBst(root);
+        return bst.kthSmallest(k);
+    }
+    class MyBst {
+        TreeNode root;
+        Map<TreeNode, Integer> nodeNum;
+        
+        public MyBst(TreeNode root) {
+            this.root = root;
+            this.nodeNum = new HashMap<TreeNode, Integer>();
+            countNodeNum(root);
+        }
+        
+        // 返回二叉搜索树中第k小的元素
+        public int kthSmallest(int k) {
+            TreeNode node = root;
+            while (node != null) {
+                int left = getNodeNum(node.left);
+                if (left < k - 1) {
+                    node = node.right;
+                    k -= left + 1;
+                } else if (left == k - 1) {
+                    break;
+                } else {
+                    node = node.left;
+                }
+            }
+            return node.val;
+        }
+        
+        // 统计以node为根结点的子树的结点数
+        private int countNodeNum(TreeNode node) {
+            if (node == null) {
+                return 0;
+            }
+            nodeNum.put(node, 1 + countNodeNum(node.left) + countNodeNum(node.right));
+            return nodeNum.get(node);
+        }
+        
+        // 获取以node为根结点的子树的结点数
+        private int getNodeNum(TreeNode node) {
+            return nodeNum.getOrDefault(node, 0);
+        }
+    }
+    
+    // 方法一:中序遍历
+    public int kthSmallest1(TreeNode root, int k) {
         Deque<TreeNode> stack = new ArrayDeque<>();
         while (root != null || !stack.isEmpty()) {
             // 一左到底
@@ -59,3 +109,5 @@ public class t230 {
         }
     }
 }
+
+
